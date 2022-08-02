538225af4369cfe97abbe4f65c6a7fae6374c416
NerXXXy <nerxxxxy@gmail.com>
2022-08-02 21:25:51 +0300

    alot of info about configuration inside

---
+++ playwright.config.js
@@ -0,0 +1,49 @@
+const config = {
+    use: {
+        baseURL: 'https://react-redux.realworld.io',
+        // browserName: 'chromium',
+        headless: false ,
+        // viewport: { height: 1280, width:720 },
+        launchOptions: {
+            slowMO:50,
+        },
+       screenshot:'only-on-failure', // same as video
+       video: 'on', // on/off(default) or on failture
+       trace: 'on', // tracing !!!!
+    },
+
+    testDir:'tests',  //get tests from
+    retries: 2,  // repeats of test if fail
+    timeout: 60000,  //timeout
+
+    projects: [
+        {
+            name: 'Chromium_600x800',
+            use:{
+                browserName: 'chromium',
+                viewport: {height:800, width:600},
+            }
+        },
+
+        {
+            name: 'Chromium_1280x720',
+            use:{
+                browserName: 'chromium',
+                viewport: {height: 720 , width: 1280}
+            }
+        },
+
+        {
+            name: 'WebKit',
+            use:{
+                browserName: 'webkit'
+            }
+        },
+
+
+
+    ]  //  to check every test with different browsers and resolutions
+
+}
+
+module.exports = config
